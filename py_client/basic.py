2909ff76a2b7b912878364a93e3aee93779a2c4d
Adriel <adriel.fsantos@outlook.com>
2022-04-12 23:05:44 -0300

    Altera url para visualizar mensagem de api_home

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,7 @@
 import requests
 
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"query": "Hello world"})
-print(get_response.text)
+print(get_response.json()['message'])
 print(get_response.status_code)
