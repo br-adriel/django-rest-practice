e20316742c5f0d0b99647d3f5adfe02f8e7b3607
Adriel <adriel.fsantos@outlook.com>
2022-04-13 10:55:46 -0300

    Melhora conversao de modelo para dicionario

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,6 @@
+from pyexpat import model
+from django.forms import model_to_dict
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from products.models import Product
 
@@ -6,9 +9,6 @@ def api_home(request):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title'])
 
     return JsonResponse(data)
