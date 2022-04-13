1a322365b96603324e17b495b1ef09f20af9fed1
Adriel <adriel.fsantos@outlook.com>
2022-04-12 22:06:55 -0300

    Adiciona exemplo de consulta a api

---
+++ py_client/basic.py
@@ -0,0 +1,7 @@
+import requests
+
+endpoint = "https://httpbin.org/anything"
+
+get_response = requests.get(endpoint, json={"query": "Hello world"})
+print(get_response.json())
+print(get_response.status_code)
