ed657840ba556f73c56f0c5b864c54e28d406628
Adriel <adriel.fsantos@outlook.com>
2022-04-14 14:45:51 -0300

    Implementa request post para criar produto

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,6 +2,10 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123})
+get_response = requests.post(endpoint,
+                             json={
+                                 "title": "Abc123",
+                                 "content": "Hello world",
+                                 "price": "abc1234"})
 print(get_response.json())
 print(get_response.status_code)
