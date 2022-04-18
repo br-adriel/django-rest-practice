f5f534adc69d79b0a17a0ace4470ae4a969ac3db
Adriel <adriel.fsantos@outlook.com>
2022-04-17 23:32:52 -0300

    Adicina view para criacao de produtos

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,6 +4,22 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        print(serializer.validated_data)
+
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content == None:
+            content = title
+
+        serializer.save(content=content)
+
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
