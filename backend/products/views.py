3614b449b76a4916e5015e0c46d74e7eab00d45e
Adriel <adriel.fsantos@outlook.com>
2022-04-19 21:41:26 -0300

    Adiciona view para atualizar produto

--- backend/products/views.py
+++ backend/products/views.py
@@ -64,3 +64,15 @@ def product_alt_view(request, pk=None):
             serializer.save(content=content)
             return Response(serializer.data)
         return Response({"invalid": "not good data"}, status=400)
+
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+            ##
