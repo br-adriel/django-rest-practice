5b8d772258b74adeab7453302ba9b4a0a585eb34
Adriel <adriel.fsantos@outlook.com>
2022-04-19 21:48:53 -0300

    Adiciona rota para exclusao de produto

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.ProductListCreateAPIView.as_view()),
     path('<int:pk>/', views.ProductDetailAPIView.as_view()),
     path('<int:pk>/update', views.ProductUpdateAPIView.as_view()),
-    # path('<int:pk>/delete', views..as_view()),
+    path('<int:pk>/delete', views.ProductDestroyAPIView.as_view()),
     # path('', views.product_alt_view),
     # path('<int:pk>/', views.product_alt_view),
 ]
